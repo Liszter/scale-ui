c76bc41b251b1451bd73a2281f57491f7ac5ac73
liszter@qq.com <liszter@qq.com>
2023-01-31 08:58:54 +0800

    UTILS

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,1 +1,48 @@
+/*
+ * @Author: liszter <liszter@qq.com>
+ * @Date: 2022-11-27 16:10:57
+ * @LastEditTime: 2022-12-19 21:44:42
+ * @LastEditors: lishutao
+ * @Description: 暂无
+ * @FilePath: \scale-ui\packages\utils\index.ts
+ */
 export * from './vue';
+
+
+/**
+ * @Description: 短横线转小驼峰
+ * @Date: 2022-12-19 21:43:30
+ * @LastEditTime: Do not edit
+ * @FilePath: Do not edit
+ * @param {*} str   todo-item
+ * @returns {*} str  todoItem
+ */
+export function getCamelCase(str) {
+  var arr=str.split('-')
+  return arr.map(function(item,index){
+      if(index===0){
+          return item
+      }else{
+          return item[0].toUpperCase()+item.slice(1)
+      }
+  }).join('')
+}
+
+/**
+ * @Description: 短横线转大驼峰
+ * @Date: 2022-12-19 21:42:38
+ * @LastEditTime: Do not edit
+ * @FilePath: Do not edit
+ * @param {*} str   todo-item
+ * @returns {*} str  TodoItem
+ */
+export function getBigCamelCase(str) {
+  var arr=str.split('-')
+  return arr.map(function(item,index){
+          return item[0].toUpperCase()+item.slice(1)
+  }).join('')
+}
+
+
+
+// console.log( getCamelCase1( 'user-name' ) ); //userName
