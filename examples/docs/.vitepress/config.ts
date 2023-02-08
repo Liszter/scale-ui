30e27780e90ec299ce75d2d9a8970f73733ed303
liszter@qq.com <liszter@qq.com>
2023-02-08 11:39:50 +0800

    vitepress

--- examples/docs/.vitepress/config.ts
+++ examples/docs/.vitepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: liszter@qq.com liszter@qq.com
  * @Date: 2023-01-30 15:51:16
  * @LastEditors: liszter@qq.com liszter@qq.com
- * @LastEditTime: 2023-02-04 17:50:31
+ * @LastEditTime: 2023-02-06 10:26:13
  * @FilePath: \scale-ui\examples\docs\.vitepress\config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ export default defineConfig({
   title: "scale-ui",
   description: '大屏组件\可视化\自适应',
   lastUpdated: true,
-
+  base: '/scale-ui/',
   head: [["link", { rel: "icon", type: "image/svg+xml", href: "logo.png" }]],
   markdown: {
     theme: {
