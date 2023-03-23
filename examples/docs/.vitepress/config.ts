4d24960460d9d1971a6d73d4d12bba0643f7a977
liszter@qq.com <liszter@qq.com>
2023-03-23 11:46:31 +0800

    缓存

--- examples/docs/.vitepress/config.ts
+++ examples/docs/.vitepress/config.ts
@@ -2,7 +2,7 @@
  * @Author: liszter@qq.com liszter@qq.com
  * @Date: 2023-01-30 15:51:16
  * @LastEditors: liszter@qq.com liszter@qq.com
- * @LastEditTime: 2023-02-23 15:29:53
+ * @LastEditTime: 2023-03-13 14:31:01
  * @FilePath: \scale-ui\examples\docs\.vitepress\config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -68,6 +68,12 @@ export default defineConfig({
 
 
 
+          ]
+        },
+        {
+          text: '业务代码复用',
+          items: [
+            { text: "头部样式总结", link: "/materials/components/headers.md" },
           ]
         }
       ],
---
+++ examples/docs/components/materials/components/headers.md
@@ -0,0 +1,20 @@
+<!--
+ * @Author: liszter@qq.com liszter@qq.com
+ * @Date: 2023-03-13 14:30:14
+ * @LastEditors: liszter@qq.com liszter@qq.com
+ * @LastEditTime: 2023-03-13 15:21:25
+ * @FilePath: \scale-ui\examples\docs\components\materials\components\headers.md
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+# 大屏的头部
+
+
+<div class="header">
+
+</div>
+
+```
+
+
+
+```
